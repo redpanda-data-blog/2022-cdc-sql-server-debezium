5415e55e60fc96f2b6139e232fcbad7775773599
Donald Le <ledinhcuong99@gmail.com>
2022-08-19 06:00:52 +0700

    Remove the sql commands for creating table order

--- sql-generator.sql
+++ sql-generator.sql
@@ -23,18 +23,4 @@ INSERT INTO rooms(hotel_id,name,description,total_rooms,used_rooms,left_rooms)
 INSERT INTO rooms(hotel_id,name,description,total_rooms,used_rooms,left_rooms)
   VALUES ('pullmanhn','single room','A room for single',6,1,5);
 EXEC sys.sp_cdc_enable_table @source_schema = 'dbo', @source_name = 'rooms', @role_name = NULL, @supports_net_changes = 0;
-
--- Create some orders ...
-CREATE TABLE orders (
-  id INTEGER IDENTITY(1001,1) NOT NULL PRIMARY KEY,
-  product_id VARCHAR(255) NOT NULL,
-  number_of_items INTEGER,
-  note VARCHAR(255) NOT NULL,
-  order_date DATE NOT NULL
-);
-INSERT INTO orders(product_id,number_of_items,note,order_date)
-  VALUES ('shoebeauty1',2,'soft ones please','22-JAN-2022');
-INSERT INTO orders(product_id,number_of_items,note)
-  VALUES ('shoebeauty1',2,'soft ones please','19-AUG-2022');
-EXEC sys.sp_cdc_enable_table @source_schema = 'dbo', @source_name = 'orders', @role_name = NULL, @supports_net_changes = 0;
 GO
